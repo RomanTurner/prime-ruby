b8a84318e54703f2d697ef7a2307bf95260ce09c
Roman Turner <rn.turner@live.com>
2021-04-05 22:35:11 -0700

    define

--- prime.rb
+++ prime.rb
@@ -1,5 +1,6 @@
 # Add  code here!
 #Prime RIBy
+#A prime number (or a prime) is a natural number greater than 1 that is not a product of two smaller natural numbers. -wikipedia
 
 def prime?(num)
     num > 1 ? ((2..num-1).to_a.all? {|x| num % x != 0}) : false
